cdb23da3bc84317321a0e2ee578a8856187d0593
David Huang <daveshuang@gmail.com>
2019-06-29 21:44:00 -0700

    Covert to es6 statements

--- liri.js
+++ liri.js
@@ -14,21 +14,17 @@ const showMovieInfo = queryUrl => {
     axios.get(queryUrl)
         .then(
             function (response, error) {
-                if (error) {
-                    console.log(error);
-                }
-                else if (param === undefined) {
-                    console.log(`If you haven't watched "Mr. Nobody", then you should: <http://www.imdb.com/title/tt0485947/>\nIt's on Netflix!`);
-                }
-                else {
-                    console.log(`Title of the movie is: ${response.data.Title}`);
-                    console.log(`Year the movie came out is: ${response.data.Year}`);
-                    console.log(`Rotten Tomatoes Rating of ${response.data.Title} is: ${response.data.Ratings[1].Value}`);
-                    console.log(`Country where the movie was produced is: ${response.data.Country}`);
-                    console.log(`Language of the movie is: ${response.data.Language}`);
-                    console.log(`Plot of the movie is: ${response.data.Plot}`);
-                    console.log(`Actors in the movie are: ${response.data.Actors}`);
-                }
+                error ? console.log(error)
+                    : param === undefined ? console.log(`If you haven't watched "Mr. Nobody", then you should: <http://www.imdb.com/title/tt0485947/>\nIt's on Netflix!`)
+                        : (
+                            console.log(`Title of the movie is: ${response.data.Title}`),
+                            console.log(`Year the movie came out is: ${response.data.Year}`),
+                            console.log(`Rotten Tomatoes Rating of ${response.data.Title} is: ${response.data.Ratings[1].Value}`),
+                            console.log(`Country where the movie was produced is: ${response.data.Country}`),
+                            console.log(`Language of the movie is: ${response.data.Language}`),
+                            console.log(`Plot of the movie is: ${response.data.Plot}`),
+                            console.log(`Actors in the movie are: ${response.data.Actors}`)
+                        );
             }
         )
 }
@@ -45,24 +41,21 @@ const showSongInfo = param => {
             limit: '1'
         },
         function (err, data) {
-            if (err) {
-                console.log(err);
-            }
-            else {
-                const songs = data.tracks.items;
-                console.log('============SONG INFO============');
-                fs.appendFileSync('log.txt', '============SONG INFO============\n');
-                console.log(`Song name: ${songs[0].name}`);
-                fs.appendFileSync('log.txt', `song name: ${songs[0].name}\n`);
-                console.log(`Preview song: ${songs[0].preview_url}`);
-                fs.appendFileSync('log.txt', `preview song: ${songs[0].preview_url}\n`);
-                console.log(`Album: ${songs[0].album.name}`);
-                fs.appendFileSync('log.txt', `album: ${songs[0].album.name}\n`);
-                console.log(`Artist(s): ${songs[0].artists[0].name}`);
-                fs.appendFileSync('log.txt', `artist(s): ${songs[0].artists[0].name}\n`);
-                console.log('=============================');
-                fs.appendFileSync('log.txt', '=============================');
-            }
+            err ? console.log(err) : (
+                songs = data.tracks.items,
+                console.log('============SONG INFO============'),
+                fs.appendFileSync('log.txt', '============SONG INFO============\n'),
+                console.log(`Song name: ${songs[0].name}`),
+                fs.appendFileSync('log.txt', `song name: ${songs[0].name}\n`),
+                console.log(`Preview song: ${songs[0].preview_url}`),
+                fs.appendFileSync('log.txt', `preview song: ${songs[0].preview_url}\n`),
+                console.log(`Album: ${songs[0].album.name}`),
+                fs.appendFileSync('log.txt', `album: ${songs[0].album.name}\n`),
+                console.log(`Artist(s): ${songs[0].artists[0].name}`),
+                fs.appendFileSync('log.txt', `artist(s): ${songs[0].artists[0].name}\n`),
+                console.log('============================='),
+                fs.appendFileSync('log.txt', '=============================')
+            );
         }
     );
 };
@@ -85,7 +78,7 @@ function readTxtFile() {
             data = data.split(","),
             option = data[0],
             param = data[1],
-            
+
             // Call query on provided params
             switchStatements(option, param)
         )
